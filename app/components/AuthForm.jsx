c1f36a4bc164223c0ecd73b91c010645e819b462
Vincent <vincentchen8093@example.com>
2024-02-21 15:25:20 +0700

    update base url

--- app/components/AuthForm.jsx
+++ app/components/AuthForm.jsx
@@ -12,7 +12,7 @@ export default function AuthForm() {
         showLinks = {false}
         view="magic_link"
         providers={[]}
-        redirectTo="https://watch-list-inky.vercel.app/auth/callback"
+        redirectTo="https://watch-list-hu52nn3va-vincents-projects-464deaf1.vercel.app/auth/callback"
         appearance = {{
             theme : 'dark',
             button: {
