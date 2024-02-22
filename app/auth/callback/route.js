111d670d446d9bde5091c1c5318c3af100987c20
Vincent <vincentchen8093@example.com>
2024-02-22 20:48:30 +0700

    update base url

--- app/auth/callback/route.js
+++ app/auth/callback/route.js
@@ -16,36 +16,22 @@
 //     return NextResponse.redirect(new URL('/watch-list', req.url))
 // }
 
+import {createRouteHandlerClient} from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers"
+import {NextResponse} from "next/server" 
 
+export async function GET(req){
+    const cookieStore = cookies()
+    const supabase = createRouteHandlerClient({cookies : () => cookieStore})
 
-"use client"
+    // Correctly parse the URL and extract the 'code' parameter
+    const url = new URL(req.url)
+    const code = url.searchParams.get('code') // Assuming the parameter is named 'code'
 
-import {Auth} from "@supabase/auth-ui-react"
-import {createClientComponentClient} from "@supabase/auth-helpers-nextjs"
+    if(code) {
+        await supabase.auth.exchangeCodeForSession(code)
+    }
 
-export default function AuthForm() {
-    const supabase = createClientComponentClient();
-
-    // Placeholder for potential error state
-    // const [error, setError] = useState(null);
-
-    return(
-        <Auth
-            supabaseClient={supabase}
-            showLinks={false}
-            view="magic_link"
-            providers={[]}
-            redirectTo="https://watch-list-inky.vercel.app/auth/callback"
-            appearance={{
-                theme: 'dark',
-                button: {
-                    className: 'bg-white/40 text-gray-900 hover:bg-gray-600' // Adjusted for clarity
-                },
-                input: {
-                    className: 'bg-gray-700 border-gray-600 text-white'
-                }
-            }}
-            // onError={(error) => setError(error.message)} // Example error handling
-        />
-    );
+    // Redirect to '/watch-list'
+    return NextResponse.redirect(new URL('/watch-list', req.url))
 }
