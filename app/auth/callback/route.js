56aa23db2d9db8bd87856c68256a732a28c9cb17
Vincent <vincentchen8093@example.com>
2024-02-22 19:36:55 +0700

    update base url

--- app/auth/callback/route.js
+++ app/auth/callback/route.js
@@ -15,24 +15,3 @@ export async function GET(req){
 
     return NextResponse.redirect(new URL('/watch-list', req.url))
 }
-
-
-// import {createRouteHandlerClient} from "@supabase/auth-helpers-nextjs"
-// import { cookies } from "next/headers"
-// import {NextResponse} from "next/server" 
-
-// export async function GET(req){
-//     const cookieStore = cookies()
-//     const supabase = createRouteHandlerClient({cookies : () => cookieStore})
-
-//     // Correctly parse the URL and extract the 'code' parameter
-//     const url = new URL(req.url)
-//     const code = url.searchParams.get('code') // Assuming the parameter is named 'code'
-
-//     if(code) {
-//         await supabase.auth.exchangeCodeForSession(code)
-//     }
-
-//     // Redirect to '/watch-list'
-//     return NextResponse.redirect(new URL('/watch-list', req.url))
-// }
